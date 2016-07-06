2ffd2f87f0894b6f16fadf87f92d80e496d02587
David Babaioff <davidbabaioff@gmail.com>
2016-07-06 22:40:24 +0300

    Fix disconnect/ add box colors

--- server/index.js
+++ server/index.js
@@ -7,28 +7,34 @@ app.get('/', function(req, res){
     res.sendFile(path.resolve('../client/index.html'));
 });
 
+app.use(require('express').static('../client'));
+
 var players  = [];
 var playerId = 0;
 
 io.on('connection', function(socket) {
     socket.on('new_player', function(data) {
-        console.log('NEW PLAYER');
-
         data.id = socket.id;
 
         players[playerId] = data;
         playerId++;
 
+        console.log('NEW PLAYER', players);
+
         io.emit('players_data', players);
     })
 
     socket.on('player_move', function(data) {
+        if (! players.length) {
+            return;
+        }
+
         for (var i = 0, length = players.length; i < length; i++) {
             if (players[i].id === socket.id) {
                 players[i].x = data.pos.x;
                 players[i].y = data.pos.y;
 
-                console.log('Client: ' + socket.id + ' | X: ' + data.pos.x + ' Y: ' + data.pos.y);
+                // console.log('Client: ', data);
                 break;
             }
         }
@@ -36,13 +42,15 @@ io.on('connection', function(socket) {
         io.emit('players_data', players);
     });
 
-    // socket.on('disconnect', function() {
-    //     for (var index = 0, length = players.length; index < length; index++) {
-    //         if (players[index].id === socket.id) {
-    //             players.splice(index, 1);
-    //         }
-    //     }
-    // });
+    socket.on('disconnect', function() {
+        players = players.filter(function(player, index) {
+            return (player.id !== socket.id);
+        });
+
+        if (playerId > 0) {
+            playerId--;
+        }
+    });
 });
 
 http.listen(3000, function(){
